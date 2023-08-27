1c4b9cadb2ad41db62e157ada546e8b695e71612
olarich11 <olarich11@gamil.com>
2023-08-27 12:30:49 -0700

    factors veryhard

--- factors.h
+++ factors.h
@@ -1,18 +1,15 @@
-#ifndef FACTORS_H
-#define FACTORS_H
-#define _GNU_SOURCE
+# ifndef FACTORS_H
+# define FACTORS_H
+# define _GNU_SOURCE
 
-#include <vector>
-#include <string>
-#include <utility>
-
+# include <vector>
+# include <string>
+# include <utility>
 
 int pollard_rho_factorization(int number);
-void main_pollard_rho(const std::string & file_path);
-
-
+void factorize_file(const std::strings&file_path);
+void main_pollard_rho(const std::string& file_path);
 int find_prime_factor(int n);
 bool is_probable_prime(int num, int tests = 5);
 void main_rsa_factorizer(const std::string & file_path);
-
 #endif /* FACTORS_H */
