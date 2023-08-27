20c59d60d42ef3a71fe3a517273685e757c0e30f
olarich11 <olarich11@gamil.com>
2023-08-27 11:13:28 -0700

    Tasks 0

---
+++ factors.h
@@ -0,0 +1,17 @@
+#ifndef FACTORS_H
+#define FACTORS_H
+
+#include <vector>
+#include <string>
+#include <utility>
+
+
+int pollard_rho_factorization(int number);
+void main_pollard_rho(const std::string & file_path);
+
+
+int find_prime_factor(int n);
+bool is_probable_prime(int num, int tests = 5);
+void main_rsa_factorizer(const std::string & file_path);
+
+#endif /* FACTORS_H */
